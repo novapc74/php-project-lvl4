5a3cc243734556f55ab75aaf9d7d435a84943e63
Makarov Vyacheslav <novapc@ukr.net>
2021-11-09 17:10:35 +0200

    add param createdBy to Model Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed $createdBy
+ */
 class Task extends Model
 {
     use HasFactory;
